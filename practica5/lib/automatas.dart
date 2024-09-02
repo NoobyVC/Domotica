efde6aa25d374e38743afff7601bf3c144f150c6
NoobyVC <yael_offhunterstar@hotmail.com>
2024-09-02 08:16:54 -0600

    Intento de imagen responsivo con;
    fit: BoxFit.cover (mas o menos sirve)

--- practica5/lib/automatas.dart
+++ practica5/lib/automatas.dart
@@ -10,6 +10,16 @@ class Automatas extends StatelessWidget {
         title: Text('Otra pagina 💀'),
         backgroundColor: Colors.black38,
       ),
+      body: Stack(
+        children: [
+          Image.network(
+            'https://4.bp.blogspot.com/-mk-XSwvjSL8/WeRJNaNTYYI/AAAAAAAAA4c/kgcctqbZuF8HrXQU44B02sWsaqiwZjl0QCLcBGAs/s640/robotPensando.jpg',
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+          ),
+        ],
+      ),
     );
   }
 }
