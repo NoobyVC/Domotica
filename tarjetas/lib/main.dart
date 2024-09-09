3ebbd320a6e4bcc22c54144545e76043eae49089
NoobyVC <yael_offhunterstar@hotmail.com>
2024-09-09 07:32:33 -0600

    Vamos a ver tarjetas :3

--- tarjetas/lib/main.dart
+++ tarjetas/lib/main.dart
@@ -1,1 +1,33 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tarjetas',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+          backgroundColor: Colors.blue,
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
+
+class cardimage extends StatelessWidget {
+  const cardimage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
