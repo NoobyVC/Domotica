6660b824c8d09b59f847a96e21f6402ea8e0550d
NoobyVC <yael_offhunterstar@hotmail.com>
2024-09-11 07:30:28 -0600

    Nueva practica, siuuuuuuuu

--- tarjetas/lib/main.dart
+++ tarjetas/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
             backgroundColor: Colors.blue,
           ),
           body: ListView(
-            children: [cardimage()],
+            children: [cardimage(), cardimagebutton()],
           )),
     );
   }
@@ -38,7 +38,38 @@ class cardimage extends StatelessWidget {
                 'https://upload.wikimedia.org/wikipedia/commons/4/47/CUC_LOGO.png'),
             height: 240,
             fit: BoxFit.cover,
+          ),
+          Text(
+            'Card con imagen del Centro',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
           )
+        ],
+      ),
+    );
+  }
+}
+
+class cardimagebutton extends StatelessWidget {
+  const cardimagebutton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      margin: EdgeInsets.all(5),
+      child: Column(
+        children: [
+          Image.network(
+            'https://images.unsplash.com/photo-1503256207526-0d5d80fa2f47?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZG9nfGVufDB8fDB8fHww',
+            width: double.infinity,
+            height: 240,
+            fit: BoxFit.cover,
+          ),
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Text('Hola'),
+          ),
+          ElevatedButton(onPressed: () {}, child: Text('Hola 2'))
         ],
       ),
     );
