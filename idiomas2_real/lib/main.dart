c44308fd3c8e906fe69b94aa4539652792eef52a
NoobyVC <yael_offhunterstar@hotmail.com>
2024-09-25 07:31:18 -0600

    Se crea otra clase StatefulWidget, se bora el scafold de la principal y se manda a llamar a la clase en home: "clase()"

--- idiomas2_real/lib/main.dart
+++ idiomas2_real/lib/main.dart
@@ -7,15 +7,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+    return MaterialApp(title: 'Material App', home: MenuPrincipal());
+  }
+}
+
+class MenuPrincipal extends StatefulWidget {
+  const MenuPrincipal({super.key});
+
+  @override
+  State<MenuPrincipal> createState() => _MenuPrincipalState();
+}
+
+class _MenuPrincipalState extends State<MenuPrincipal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('uwu'),
       ),
     );
   }
