f26e58b029bfdcede7f01008e30e02ab1b3331dc
NoobyVC <yael_offhunterstar@hotmail.com>
2024-10-09 07:37:42 -0600

    Bueno, se agrego la clase statefulWidget y se mando a llamar en la rama principal, pues esta clase sirve cuando los objetos a ver cambiaran constantemente.
    En el scaffold se aprecian cosas sencillas, nada del otro mundo

--- voz2/lib/main.dart
+++ voz2/lib/main.dart
@@ -8,13 +8,38 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
+      debugShowCheckedModeBanner: false,
+      title: 'Asistente Voz uwu',
+      home: AsistenteVoz(),
+    );
+  }
+}
+
+class AsistenteVoz extends StatefulWidget {
+  const AsistenteVoz({super.key});
+
+  @override
+  State<AsistenteVoz> createState() => _AsistenteVozState();
+}
+
+class _AsistenteVozState extends State<AsistenteVoz> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mira nomas que pro este asisente'),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: "Bienvenute"),
+            )
+          ],
         ),
       ),
     );
